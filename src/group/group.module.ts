44a1474d5983d8207deffde9ef62fca511889dbc
Artem Melnyk <melkam464@gmail.com>
2022-04-08 18:25:27 +0200

    ci: husky and commitizen configuration

--- src/group/group.module.ts
+++ src/group/group.module.ts
@@ -3,7 +3,7 @@ import { GroupService } from './group.service';
 import { GroupController } from './group.controller';
 
 @Module({
-  providers: [GroupService],
-  controllers: [GroupController]
+	providers: [GroupService],
+	controllers: [GroupController],
 })
 export class GroupModule {}
