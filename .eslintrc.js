c30f6374237dda277877d75ce7748ba47d003c26
Artem Melnyk <melkam464@gmail.com>
2022-04-24 10:05:37 +0200

    ci(.eslintrc.js): eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
 		'@typescript-eslint/no-explicit-any': 'off',
 		'@typescript-eslint/no-unused-vars': [
-			'warn',
+			'error',
 			{ argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' },
 		],
 		'no-console': ['warn', { allow: ['warn', 'error'] }],
